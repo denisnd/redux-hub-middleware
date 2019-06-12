b8bf1e6756783a8c5c255a1a80ebfb764f015426
Denis Nasyrtdinov <denisnd@yandex.ru>
2019-06-12 12:55:05 +0700

    initial

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import invariant from "invariant";
+
+const wrapAction = action => ({ ...action, __hubOriginated: true });
+
+const isActionHubOriginated = action => !!action.__hubOriginated;
+
+const unwrapAction = action => {
+    const { __hubOriginated, ...rest } = action;
+    return rest;
+};
+
+export default function createHubMiddleware() {
+    const connectedStores = [];
+
+    const connect = store => connectedStores.push(store);
+
+    const middleware = store => next => action => {
+        if (connectedStores.length < 2) {
+            return next(action);
+        }
+
+        if (isActionHubOriginated(action)) {
+            return next(unwrapAction(action));
+        }
+
+        const index = connectedStores.findIndex(
+            connectedStore => store.getState === connectedStore.getState
+        );
+
+        invariant(
+            index !== -1,
+            "This store is not connected to hub, but hub middleware is used in it"
+        );
+
+        const result = next(action);
+
+        connectedStores.forEach((connectedStore, storeIndex) => {
+            if (storeIndex === index) return;
+            connectedStore.dispatch(wrapAction(action));
+        });
+
+        return result;
+    };
+
+    return {
+        connect,
+        middleware
+    };
+}
