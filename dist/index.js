65f712fe5a66806c2f673f771580b59d23cd56f4
Denis Nasyrtdinov <denisnd@yandex.ru>
2019-06-12 15:02:22 +0700

    invariant removed from deps 2 space indent

--- dist/index.js
+++ dist/index.js
@@ -1,74 +1,79 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-exports.default = createHubMiddleware;
-
-var _invariant = require("invariant");
-
-var _invariant2 = _interopRequireDefault(_invariant);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+exports.default = createReduxHub;
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 var wrapAction = function wrapAction(action) {
-    return _extends({}, action, { __hubOriginated: true });
+  return _extends({}, action, { __hubOriginated: true });
 };
 
 var isActionHubOriginated = function isActionHubOriginated(action) {
-    return !!action.__hubOriginated;
+  return !!action.__hubOriginated;
 };
 
 var unwrapAction = function unwrapAction(action) {
-    var __hubOriginated = action.__hubOriginated,
-        rest = _objectWithoutProperties(action, ["__hubOriginated"]);
+  var __hubOriginated = action.__hubOriginated,
+      rest = _objectWithoutProperties(action, ["__hubOriginated"]);
 
-    return rest;
+  return rest;
 };
 
-function createHubMiddleware() {
-    var connectedStores = [];
+var invariant = function invariant(condition, message) {
+  if (!condition) {
+    var error = new Error();
 
-    var connect = function connect(store) {
-        return connectedStores.push(store);
+    error.name = 'Invariant violation';
+    error.message = message;
+
+    throw error;
+  }
+};
+
+function createReduxHub() {
+  var connectedStores = [];
+
+  var connect = function connect(store) {
+    return connectedStores.push(store);
+  };
+
+  var middleware = function middleware(store) {
+    return function (next) {
+      return function (action) {
+        if (connectedStores.length < 2) {
+          return next(action);
+        }
+
+        if (isActionHubOriginated(action)) {
+          return next(unwrapAction(action));
+        }
+
+        var index = connectedStores.findIndex(function (connectedStore) {
+          return store.getState === connectedStore.getState;
+        });
+
+        invariant(index !== -1, "This store is not connected to a hub, but hub middleware is used in it");
+
+        var result = next(action);
+
+        connectedStores.forEach(function (connectedStore, storeIndex) {
+          if (storeIndex === index) return;
+          connectedStore.dispatch(wrapAction(action));
+        });
+
+        return result;
+      };
     };
+  };
 
-    var middleware = function middleware(store) {
-        return function (next) {
-            return function (action) {
-                if (connectedStores.length < 2) {
-                    return next(action);
-                }
-
-                if (isActionHubOriginated(action)) {
-                    return next(unwrapAction(action));
-                }
-
-                var index = connectedStores.findIndex(function (connectedStore) {
-                    return store.getState === connectedStore.getState;
-                });
-
-                (0, _invariant2.default)(index !== -1, "This store is not connected to hub, but hub middleware is used in it");
-
-                var result = next(action);
-
-                connectedStores.forEach(function (connectedStore, storeIndex) {
-                    if (storeIndex === index) return;
-                    connectedStore.dispatch(wrapAction(action));
-                });
-
-                return result;
-            };
-        };
-    };
-
-    return {
-        connect: connect,
-        middleware: middleware
-    };
+  return {
+    connect: connect,
+    middleware: middleware
+  };
 }
--- src/index.js
+++ src/index.js
@@ -1,49 +1,58 @@
-import invariant from "invariant";
-
 const wrapAction = action => ({ ...action, __hubOriginated: true });
 
 const isActionHubOriginated = action => !!action.__hubOriginated;
 
 const unwrapAction = action => {
-    const { __hubOriginated, ...rest } = action;
-    return rest;
+  const { __hubOriginated, ...rest } = action;
+  return rest;
+};
+
+const invariant = (condition, message) => {
+  if (!condition) {
+    const error = new Error();
+
+    error.name = 'Invariant violation';
+    error.message = message;
+
+    throw error;
+  }
 };
 
 export default function createReduxHub() {
-    const connectedStores = [];
+  const connectedStores = [];
 
-    const connect = store => connectedStores.push(store);
+  const connect = store => connectedStores.push(store);
 
-    const middleware = store => next => action => {
-        if (connectedStores.length < 2) {
-            return next(action);
-        }
+  const middleware = store => next => action => {
+    if (connectedStores.length < 2) {
+      return next(action);
+    }
 
-        if (isActionHubOriginated(action)) {
-            return next(unwrapAction(action));
-        }
+    if (isActionHubOriginated(action)) {
+      return next(unwrapAction(action));
+    }
 
-        const index = connectedStores.findIndex(
-            connectedStore => store.getState === connectedStore.getState
-        );
+    const index = connectedStores.findIndex(
+      connectedStore => store.getState === connectedStore.getState
+    );
 
-        invariant(
-            index !== -1,
-            "This store is not connected to a hub, but hub middleware is used in it"
-        );
+    invariant(
+      index !== -1,
+      "This store is not connected to a hub, but hub middleware is used in it"
+    );
 
-        const result = next(action);
+    const result = next(action);
 
-        connectedStores.forEach((connectedStore, storeIndex) => {
-            if (storeIndex === index) return;
-            connectedStore.dispatch(wrapAction(action));
-        });
+    connectedStores.forEach((connectedStore, storeIndex) => {
+      if (storeIndex === index) return;
+      connectedStore.dispatch(wrapAction(action));
+    });
 
-        return result;
-    };
+    return result;
+  };
 
-    return {
-        connect,
-        middleware
-    };
+  return {
+    connect,
+    middleware
+  };
 }
